7a58c93222490c4ed0ad284c2b277af7388507e6
tomoyuki_HAYAKAWA <x14091xx@aitech.ac.jp>
2017-04-23 15:59:56 +0900

    Initial Commit

---
+++ blockblockbattle02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blockblockbattle02
+//
+//  Created by Tomoyuki Hayakawa on 2017/04/23.
+//  Copyright © 2017年 Tomoyuki Hayakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
